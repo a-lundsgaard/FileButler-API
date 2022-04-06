a6f1f8ae417c86382e30887185ba87add5a1ff12
a-lundsgaard <askelundsgaard@live.dk>
2022-04-06 22:34:04 +0200

    update

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -1,11 +1,11 @@
 echo "Building docker image for linux/amd64\n"
-docker buildx build --platform linux/amd64 -t ocr-api . &&
+docker buildx build --platform linux/amd64 -t ocr-api-1 . &&
 
 echo "\nTagging image for registry\n"
-docker tag ocr-api registry.heroku.com/ocr-api/web &&
+docker tag ocr-api-1 registry.heroku.com/ocr-api-1/web &&
 
 echo "\nPushing docker image for registry container\n"
-docker push registry.heroku.com/ocr-api/web &&
+docker push registry.heroku.com/ocr-api-1/web &&
 
 echo "\nReleasing build to production\n"
-heroku container:release web -a ocr-api
+heroku container:release web -a ocr-api-1
